8b07d88add6bfa441ab5e15e49dedb0f3f150f0b
Marco Franke <mfranke87@icloud.com>
2026-01-17 11:03:41 +0100

    fix: set base path to root for development environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/spoolman-filament-swatch/' : '/',
+  base: '/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
