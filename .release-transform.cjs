2b9923ebb8ef86f05819f0aac003dfb61f55677b
Marco Franke <mfranke87@icloud.com>
2026-01-17 12:22:32 +0100

    feat: enhance release transform function with author name and refactor hash shortening

--- .release-transform.cjs
+++ .release-transform.cjs
@@ -1,9 +1,17 @@
 module.exports = {
     transform: (commit, context) => {
-        // Shorten hash to 7 characters and return a new object
+        // Shorten hash to 7 characters
+        const shortHash = commit.hash ? commit.hash.substring(0, 7) : commit.hash;
+
+        // Use author name from commit
+        const authorName = commit.author?.name || commit.committer?.name || 'unknown';
+
         return {
             ...commit,
-            shortHash: commit.hash ? commit.hash.substring(0, 7) : commit.hash
+            shortHash,
+            authorName
         };
-        `
+    },
+    commitPartial: `* {{#if scope}}**{{scope}}:** {{/if}}{{subject}} ([{{shortHash}}]({{@root.host}}/{{@root.owner}}/{{@root.repository}}/commit/{{hash}})){{#if authorName}} by @{{authorName}}{{/if}}
+`
 };
