da9e9fb3d5fd232a44cd29e984a2d563250b83f6
Marco Franke <mfranke87@icloud.com>
2026-01-17 12:20:24 +0100

    fix: streamline hash shortening in release transform function

--- .release-transform.cjs
+++ .release-transform.cjs
@@ -1,12 +1,9 @@
 module.exports = {
     transform: (commit, context) => {
-        // Shorten hash to 7 characters
-        if (commit.hash) {
-            commit.shortHash = commit.hash.substring(0, 7);
-        }
-
-        return commit;
-    },
-    commitPartial: `* {{#if scope}}**{{scope}}:** {{/if}}{{subject}} ([{{shortHash}}]({{@root.host}}/{{@root.owner}}/{{@root.repository}}/commit/{{hash}})){{#if committerName}} by @{{committerName}}{{/if}}
-`
+        // Shorten hash to 7 characters and return a new object
+        return {
+            ...commit,
+            shortHash: commit.hash ? commit.hash.substring(0, 7) : commit.hash
+        };
+        `
 };
